d2bc40a06d9a985c34ead5e78799fb32c6e448d8
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-04 16:08:03 +0300

    render category name on each page

--- lib/category_cook_screen.dart
+++ lib/category_cook_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryCookScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryCookScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Category'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The recepies for category!'),
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,13 +2,14 @@ import './category_cook_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryCookScreen();
+      return CategoryCookScreen(id, title);
     }));
   }
 
